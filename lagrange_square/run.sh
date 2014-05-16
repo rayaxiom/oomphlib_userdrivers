56b6d800667019c0812d1f60aced62c6f7413277
Raymon White <raymonwhite42@gmail.com>
2014-05-16 12:03:38 +0100

    rayheader.h: added more comments and tidied up more

--- lagrange_square/run.sh
+++ lagrange_square/run.sh
@@ -5,13 +5,17 @@ CURRENTDIR=`pwd`
 # --f_amg_str is 0.668 for stress divergence or 0.25 for simple
 #
 # --f_coarse:
-# 0 - CLJP
-# 1 - Classical RS
-# 3 - modified RS
-# 6 - Falgout - default on documentation
-# 8 - PMIS
-# 10 - HMIS
-# 11 - One pass on RS coarsening on each processor, not recommended.
+# 0 = CLJP (parallel coarsening using independent sets)
+# 1 = classical RS with no boundary treatment (not recommended
+#     in parallel)
+# 3 = modified RS with 3rd pass to add C points on the boundaries
+# 6 = Falgout (uses 1 then CLJP using interior coarse points as
+#     first independent set)
+# 8 = PMIS (parallel coarsening using independent sets - lower
+#     complexities than 0, maybe also slower convergence)
+# 10= HMIS (one pass RS on each processor then PMIS on interior
+#     coarse points as first independent set)
+# 11= One pass RS on each processor (not recommended)
 #
 # --f_amg_sim_smoo:
 # 0 - Jacobi (Need to set damping as well) (damping needed)
@@ -30,6 +34,7 @@ CURRENTDIR=`pwd`
 # --f_amg_damp should only be positive for Jacobi smoother, (f_amg_sim_smoo 0)
 # if we are not using Jacobi for smoothing, then the damping value is ignored.
 #
+#
 # --f_solver 69 is equivalent to these settings:
 #FAMG_ITER="--f_amg_iter 1"
 #FAMG_SMITER="--f_amg_smiter 2"
@@ -44,14 +49,14 @@ FAMG_SMITER="--f_amg_smiter 2"
 FAMG_SSMOOTHER=""
 FAMG_CSMOOTHER="--f_amg_com_smoo 9"
 FAMG_DAMP="--f_amg_damp 0.5"
-FAMG_STRN="--f_amg_str 0.668"
+FAMG_STRN="--f_amg_str 0.25" # REMEMBER TO CHANGE THIS FOR SIMPLE/STRESS VISCOUS FORMS.
 FAMG_COARSE="--f_amg_coarse 1"
 
 FAMGPARAM="--f_solver 96 $FAMG_ITER $FAMG_SMITER $FAMG_SSMOOTHER $FAMG_CSMOOTHER $FAMG_DAMP $FAMG_STRN $FAMG_COARSE"
 #FAMGPARAM="--f_solver 69"
 
 make sq_lgr && mpirun -np 1 ./sq_lgr --dist_prob --trilinos_solver --prob_id 11 --w_solver 0 \
-  --ns_solver 1 --p_solver 1 $FAMGPARAM --visc 1 --ang 67 --rey 200 --noel 128 \
+  --ns_solver 1 --p_solver 1 $FAMGPARAM --visc 0 --ang 67 --rey 200 --noel 175 \
 	--print_hypre
 
 #  --w_solver 1 --ns_solver 0 --ang 42 --noel 3 --doc_prec tmp_doc_prec_dir
