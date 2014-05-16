ad1a5801f6f0680cc1117468452793791198fac7
Raymon White <raymonwhite42@gmail.com>
2014-05-16 10:03:58 +0100

    Fixed up rayheader.h - works properly now.
    Fixed a typo where I wrote FAMF_CSMOOTHER instead of FAMG__CSMOOTHER
    Generally tidied up a lot of stuff and deleted old code.

--- lagrange_square/run.sh
+++ lagrange_square/run.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
-
 CURRENTDIR=`pwd`
-#cd ../../src/ && make && make install && cd $CURRENTDIR && \
-#make sq_lgr && mpirun -np 2 sq_lgr --prob_id 11 --w_solver 0 --ns_solver 0 --visc 1 --ang 42 --rey 100 --noel 3
-
-#make sq_lgr && mpirun -np 1 ./sq_lgr --dist_prob --prob_id 11 \
-#  --doc_soln tmp_soln_dir --itstimedir tmp_its_dir \
-#  --visc 0 --rey 100 \
-#  --w_solver 0 --ns_solver 0 --ang 42 --noel 3 --doc_prec tmp_doc_prec_dir \
-#  --bdw --sigma 42.001
-
-#make sq_lgr && mpirun -np 1 ./sq_lgr --dist_prob --prob_id 11 \
-#  --doc_soln tmp_soln_dir --itstimedir tmp_its_dir \
-#  --visc 0 --rey 100 \
-#  --w_solver 0 --ns_solver 0 --ang 42 --noel 3 --doc_prec tmp_doc_prec_dir \
-#  --bdw --sigma 42.001
-
-#make sq_lgr && mpirun -np 1 ./sq_lgr --dist_prob --prob_id 11 \
-#  --itstimedir tmp_its_dir \
-#  --visc 1 --rey 100 \
-#  --w_solver 0 --ns_solver 1 --f_solver 0 --p_solver 0 --ang 42 --noel 8
-
-
 
 # --f_amg_iter is generally 1, --f_amg_smiter is generally 2
 # --f_amg_str is 0.668 for stress divergence or 0.25 for simple
@@ -44,7 +22,6 @@ CURRENTDIR=`pwd`
 # 6 - hybrid symmetric GS or SSOR. (damping needed ?)
 #
 # --f_amg_com_smoo:
-#
 # 6 - Schwarz
 # 7 - Pilut
 # 8 - ParaSails
@@ -52,17 +29,29 @@ CURRENTDIR=`pwd`
 #
 # --f_amg_damp should only be positive for Jacobi smoother, (f_amg_sim_smoo 0)
 # if we are not using Jacobi for smoothing, then the damping value is ignored.
+#
+# --f_solver 69 is equivalent to these settings:
+#FAMG_ITER="--f_amg_iter 1"
+#FAMG_SMITER="--f_amg_smiter 2"
+#FAMG_SSMOOTHER="--f_amg_sim_smoo 1"
+#FAMG_CSMOOTHER=""
+#FAMG_DAMP="--f_amg_damp -1"
+#FAMG_STRN="--f_amg_str 0.668"
+#FAMG_COARSE="--f_amg_coarse 1"
+
 FAMG_ITER="--f_amg_iter 1"
 FAMG_SMITER="--f_amg_smiter 2"
-FAMG_SSMOOTHER="--f_amg_sim_smoo 1"
-FAMG_CSMOOTHER=""
-FAMG_DAMP="--f_amg_damp -1"
+FAMG_SSMOOTHER=""
+FAMG_CSMOOTHER="--f_amg_com_smoo 9"
+FAMG_DAMP="--f_amg_damp 0.5"
 FAMG_STRN="--f_amg_str 0.668"
 FAMG_COARSE="--f_amg_coarse 1"
 
-FAMGPARAM="$FAMG_ITER $FAMG_SMITER $FAMG_SSMOOTHER $FAMF_CSMOOTHER $FAMG_DAMP $FAMG_STRN $FAMG_COARSE"
-make sq_lgr && mpirun -np 1 ./sq_lgr --prob_id 11 --w_solver 0 \
-  --ns_solver 1 --p_solver 1 --f_solver 96 $FAMGPARAM --visc 1 --ang 67 --rey 200 --noel 128 \
+FAMGPARAM="--f_solver 96 $FAMG_ITER $FAMG_SMITER $FAMG_SSMOOTHER $FAMG_CSMOOTHER $FAMG_DAMP $FAMG_STRN $FAMG_COARSE"
+#FAMGPARAM="--f_solver 69"
+
+make sq_lgr && mpirun -np 1 ./sq_lgr --dist_prob --trilinos_solver --prob_id 11 --w_solver 0 \
+  --ns_solver 1 --p_solver 1 $FAMGPARAM --visc 1 --ang 67 --rey 200 --noel 128 \
 	--print_hypre
 
 #  --w_solver 1 --ns_solver 0 --ang 42 --noel 3 --doc_prec tmp_doc_prec_dir
