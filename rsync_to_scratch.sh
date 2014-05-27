07a634a24f9075e62d2dbeda893e56e0757d9a22
Raymon White <raymonwhite42@gmail.com>
2014-05-27 21:44:34 +0100

    new file:   rsync_to_scratch.sh new file:   scratch_file_list.txt
    For the csf cluster

---
+++ rsync_to_scratch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+UDRI="/mnt/iusers01/mh01/mbax5ml3/oomphlib_optimized/user_drivers"
+SCRATCHUDRI="/mnt/iusers01/mh01/mbax5ml3/scratch/oomphlib_optimized/user_drivers"
+
+FILELIST="scratch_file_list.txt"
+
+rsync -av --files-from=$FILELIST $SCRATCHUDRI
+
+
+
