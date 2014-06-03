f9b618b1f580880285f416ac56f26daab8ff8373
Raymon White <raymonwhite42@gmail.com>
2014-06-03 12:22:51 +0100

    Added a test to see if the number of src and destination is equal

--- rsync_format_scripts.sh
+++ rsync_format_scripts.sh
@@ -22,13 +22,22 @@ pullonly()
 
 pullonly
 
+# Source files, INCLUDING the actual file (filename)
 declare -a OOMPH_SOURCE=(\
-"lagrange_step/format_test7_StPo_final.sh" \
-"lagrange_step/run.sh")
+"lagrange_step/format_test7_StPo_final.sh")
 
+# Destination folder (without the filename)
+# The source files will all be renamed "format_results.sh" in the destination
+# folder.
 declare -a SCRATCH_DEST=(\
-"lagrange_step/test7_StPo_final" \
 "lagrange_step/test7_StPo_final")
+
+# Check if the number of source and destination are correct (equal)
+if [ "${#OOMPH_SOURCE[@]}" -ne ${#SCRATCH_DEST[@]} ]
+then
+  echo "Number of source and target differ"
+  exit 1
+fi
 
 for ((i=0;i<${#OOMPH_SOURCE[@]};++i)); do
   cp $OOMPHOPTUDRI/${OOMPH_SOURCE[i]} $SCRATCHUDRI/${SCRATCH_DEST[i]}/format_results.sh
