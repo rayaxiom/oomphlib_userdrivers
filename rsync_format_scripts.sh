06beda80b435faad4e65a6ff5d3aa8a707af802c
Raymon White <raymonwhite42@gmail.com>
2014-06-03 11:58:10 +0100

    Added new script to rsync the format scripts to scratch

---
+++ rsync_format_scripts.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CURRENT_DIR=`pwd`
+
+OOMPHOPT="/mnt/iusers01/mh01/mbax5ml3/oomphlib_optimized"
+SCRATCH="/mnt/iusers01/mh01/mbax5ml3/scratch/oomphlib_optimized"
+
+OOMPHOPTUDRI="$OOMPHOPT/user_drivers"
+SCRATCHUDRI="$SCRATCH/user_drivers"
+
+pullonly()
+{
+  TMPDIR=`pwd`
+  cd $OOMPHOPT && \
+  git fetch --all && \
+  git reset --hard origin/master && \
+  cd $OOMPHOPTUDRI && \
+  git fetch --all && \
+  git reset --hard origin/master && \
+  cd $TMPDIR
+}
+
+pullonly
+
+declare -a OOMPH_SOURCE=(\
+"lagrange_step/format_test7_StPo_final.sh" \
+"lagrange_step/run.sh")
+
+declare -a SCRATCH_DEST=(\
+"lagrange_step/test7_StPo_final" \
+"lagrange_step/test7_StPo_final")
+
+for ((i=0;i<${#OOMPH_SOURCE[@]};++i)); do
+  rsync -av $OOMPHOPTUDRI/${OOMPH_SOURCE[i]} $SCRATCHUDRI/${SCRATCH_DEST[i]}
+done
+
+
