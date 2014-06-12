bfb46c96a635d681db8c8130a059d23ce3828056
Raymon White <raymonwhite42@gmail.com>
2014-06-12 19:38:54 +0100

    Updated validate.sh script again, mainly just formatting the echo'd outputs

--- validate.sh
+++ validate.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 USER_DRIVERS_DIR=`pwd`
-#folders=("lagrange_square" \
-#"lagrange_step")
 
 touch validation.log
 rm -rf validation.log
 touch validation.log
 
+# Get version of oomph-lib
+# The OOMPHROOT_DIR is relative to the PROGRAM_DIR
+# So we have to go into PROGRAM_DIR first (to be safe).
+echo "OOMPH-LIB version:" >> validation.log
+cd ..
+git log -1 >> $USER_DRIVERS_DIR/validation.log
+echo -e "\n" >> $USER_DRIVERS_DIR/validation.log
+cd $USER_DRIVERS_DIR
+echo "user_drivers version:" >> validation.log
+git log -1 >> $USER_DRIVERS_DIR/validation.log
+echo -e "\n" >> $USER_DRIVERS_DIR/validation.log
+
+
 for d in */ ;
 do
   if [[ -d "$d" && -f "$d/validate.sh" ]];
   then
-    echo "$d/Validate/validate.log:" >> validation.log
+    echo "$d/Validate/validation.log:" >> validation.log
 
     ## Go into the directory
     cd $d
@@ -27,6 +38,7 @@ do
     then
       cat ./Validate/validation.log >> $USER_DRIVERS_DIR/validation.log
     else
+      rm -rf ./Validate
       echo "OK" >> $USER_DRIVERS_DIR/validation.log
       rm -rf Validate validate.output
     fi
@@ -37,6 +49,11 @@ do
 done # for loop through folders.
 
 cd $USER_DRIVERS_DIR
+echo -e "\n\n"
+echo "========================================================================"
+echo "========================================================================"
+echo "cat validation.log: "
+echo "========================================================================"
 cat validation.log
 
 
