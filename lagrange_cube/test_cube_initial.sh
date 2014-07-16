24d7469b9c0c1aec613cc0a6d0411c35647e069e
Raymon White <raymonwhite42@gmail.com>
2014-07-16 07:54:27 +0100

    Fixed the test_cube_initial.sh script

--- lagrange_cube/test_cube_initial.sh
+++ lagrange_cube/test_cube_initial.sh
@@ -189,220 +189,220 @@ gen_tests_cube_amg
 
 
 
-#. $PROGRAM_DIR/../generate_qsub_script.sh
-## Do this bit in a sub shell
-#(generate_qsub_script $TEST_LIST)
-#
-#TEST_RUN="$TESTLIST_FILEBASE.sh"
-#echo "#!/bin/bash" >> $TEST_RUN
-#cat $TEST_LIST >> $TEST_RUN
-#
-#cp ./../$0 .
-#
-#QSUBFILE="$TESTLIST_FILEBASE.qsub"
-#
-#
-################################################################################
-### Produce the script to format results. 
-#function local_format_results()
-#{
-#CURRENT_DIR=`pwd`
-#
-#function format_AwPo_exacts()
-#{
-#ITSTIMEDIR="res_iterations"
-#
-#cd $ITSTIMEDIR
-#
-#TAG="RAYITS"
-##TAG="RAYPRECSETUP"
-##TAG="RAYLINSOLVER"
-#
-#LINE=""
-#
-#Prob_str="AwPo"
-#PRECLIST="WedNe WedNlFePe WedNlFrayPe WedNlFePa"
-#VISLIST="Sim Str"
-#ANGLIST="A_"
-#RELIST="R0 R100 R200 R500 R1000"
-#NOELLIST="N2 N4 N8 N16 N32 N64 N128 N256"
-#
-#for PREC in $PRECLIST
-#do
-#  for VIS in $VISLIST
-#  do
-#    for ANG in $ANGLIST
-#    do
-#      for RE in $RELIST
-#      do
-#        LINE=""
-#        for NOEL in $NOELLIST
-#        do
-#
-#          RESFILE="${Prob_str}${VIS}${RE}${PREC}${ANG}${NOEL}NP1R0"
-#          TOKEN=""
-#
-#          if [ -f $RESFILE ]
-#          then
-#            TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
-#          else
-#            TOKEN="x"
-#          fi
-#          
-#          LINE="$LINE $TOKEN"
-#        done
-#        echo $LINE
-#      done
-#    done
-#  done
-#done
-#}
-#
-#function format_AwPo_amg()
-#{
-#ITSTIMEDIR="res_iterations"
-#
-#cd $ITSTIMEDIR
-#
-#TAG="RAYITS"
-##TAG="RAYPRECSETUP"
-##TAG="RAYLINSOLVER"
-#
-#LINE=""
-#
-#Prob_str="AwPo"
-#PRECLIST="WedNlFrayPa"
-#VISLIST="Sim Str"
-#ANGLIST="A_"
-#RELIST="R0 R100 R200 R500 R1000"
-#NOELLIST="N2 N4 N8 N16 N32 N64 N128 N256 N512"
-#
-#for PREC in $PRECLIST
-#do
-#  for VIS in $VISLIST
-#  do
-#    for ANG in $ANGLIST
-#    do
-#      for RE in $RELIST
-#      do
-#        LINE=""
-#        for NOEL in $NOELLIST
-#        do
-#
-#          RESFILE="${Prob_str}${VIS}${RE}${PREC}${ANG}${NOEL}NP1R0"
-#          TOKEN=""
-#
-#          if [ -f $RESFILE ]
-#          then
-#            TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
-#          else
-#            TOKEN="x"
-#          fi
-#          
-#          LINE="$LINE $TOKEN"
-#        done
-#        echo $LINE
-#      done
-#    done
-#  done
-#done
-#}
-#
-#
-#cd $CURRENT_DIR
-#echo -e "\n"
-#echo -e "Formatting EXACTS results"
-#format_AwPo_exacts
-#
-#cd $CURRENT_DIR
-#echo -e "\n"
-#echo -e "Formatting pure AMG results"
-#format_AwPo_amg
-#}
-#
-#FORMAT_RESULTS_SCRIPT="format_results.sh"
-#echo '#!/bin/bash' >> $FORMAT_RESULTS_SCRIPT
-#declare -f local_format_results >> $FORMAT_RESULTS_SCRIPT
-#echo 'local_format_results' >> $FORMAT_RESULTS_SCRIPT
-#echo -e "\n I have created the format results script: $FORMAT_RESULTS_SCRIPT"
-#echo -e "\n"
-#
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-#################### Now check if I'm on csf, if so, delete the related scratch
-## and copy the current stuff there.
-#echo -e "\n"
-#OOMPH_ABS_ROOT_DIR=""
-#SCRATCH_ABS_ROOT_DIR=""
-#COPY_TO_SCRATCH=0
-#
-#if [[ $HOME == *mbax5ml3* ]]
-#then
-#  OOMPH_ABS_ROOT_DIR="/mnt/iusers01/mh01/mbax5ml3/oomphlib_optimized"
-#  SCRATCH_ABS_ROOT_DIR="/mnt/iusers01/mh01/mbax5ml3/scratch/oomphlib_optimized"
-#  echo "This is csf, copying files into:"
-#  echo "$SCRATCH_ABS_ROOT_DIR"
-#  COPY_TO_SCRATCH=1
-#elif [[ $HOSTNAME == *onigiri* ]]
-#then
-#  OOMPH_ABS_ROOT_DIR="/home/ray/oomphlib/matthias_bpf_rewrite"
-#  SCRATCH_ABS_ROOT_DIR="/home/ray/oomphlib/scratch/oomphlib_optimized"
-#  echo "This is onigiri, testing of files into:"
-#  echo "$SCRATCH_ABS_ROOT_DIR"
-#  COPY_TO_SCRATCH=1
-#else
-#  echo "Unrecognised machine, not doing copy into scratch."
-#  COPY_TO_SCRATCH=0
-#fi
-#
-#echo -e "\n"
-#
-## NOTE, we are still in TEST_DIR
-#if [ "$COPY_TO_SCRATCH" -eq "1" ]
-#then
-#  echo "Doing copy to scratch!"
-#
-#  cd $PROGRAM_DIR
-#  LOCAL_PROGRAM_DIR=${PWD##*/}
-#  cd $TEST_DIR
-#
-#  OOMPH_PROGRAM_DIR="$OOMPH_ABS_ROOT_DIR/user_drivers/$LOCAL_PROGRAM_DIR"
-#  SCRATCH_PROGRAM_DIR="$SCRATCH_ABS_ROOT_DIR/user_drivers/$LOCAL_PROGRAM_DIR"
-#
-#  OOMPH_TEST_DIR="$OOMPH_PROGRAM_DIR/$TEST_DIR"
-#  SCRATCH_TEST_DIR="$SCRATCH_PROGRAM_DIR/$TEST_DIR"
-#
-#  echo -e "\n"
-#  echo "OOMPH_TEST_DIR: $OOMPH_TEST_DIR"
-#  echo "SCRATCH_TEST_DIR: $SCRATCH_TEST_DIR"
-#  echo "Removing SCRATCH_TEST_DIR and recopying"
-#
-#  # Remove the scratch stuff.
-#  rm -rf $SCRATCH_TEST_DIR
-#  mkdir -p $SCRATCH_TEST_DIR
-#
-#  rsync -av $OOMPH_TEST_DIR/$PROGRAM $SCRATCH_TEST_DIR/
-#  rsync -av $OOMPH_TEST_DIR/$QSUBFILE $SCRATCH_TEST_DIR/
-#  rsync -av $OOMPH_TEST_DIR/$TEST_LIST $SCRATCH_TEST_DIR/
-#  rsync -av $OOMPH_TEST_DIR/$FORMAT_RESULTS_SCRIPT $SCRATCH_TEST_DIR/
-#
-#  ## Create the res_its and qsub output directories in scratch.
-#  mkdir -p $SCRATCH_TEST_DIR/$RESITS_DIR
-#  QSUBOUTPUT_DIR="qsub_output_$TESTLIST_FILEBASE"
-#  mkdir -p $SCRATCH_TEST_DIR/$QSUBOUTPUT_DIR
-#  echo -e "\n"
-#  echo "I have moved the files:"
-#  echo "$PROGRAM"
-#  echo "$QSUBFILE"
-#  echo "$TEST_LIST"
-#  echo "$FORMAT_RESULTS_SCRIPT"
-#  echo "and created directories:"
-#  echo "$RESITS_DIR"
-#  echo "$QSUBOUTPUT_DIR"
-#fi
-#
+. $PROGRAM_DIR/../generate_qsub_script.sh
+# Do this bit in a sub shell
+(generate_qsub_script $TEST_LIST)
+
+TEST_RUN="$TESTLIST_FILEBASE.sh"
+echo "#!/bin/bash" >> $TEST_RUN
+cat $TEST_LIST >> $TEST_RUN
+
+cp ./../$0 .
+
+QSUBFILE="$TESTLIST_FILEBASE.qsub"
+
+
+###############################################################################
+## Produce the script to format results. 
+function local_format_results()
+{
+CURRENT_DIR=`pwd`
+
+function format_cube_exacts()
+{
+ITSTIMEDIR="res_iterations"
+
+cd $ITSTIMEDIR
+
+TAG="RAYAVGITS"
+#TAG="RAYPRECSETUP"
+#TAG="RAYLINSOLVER"
+
+LINE=""
+
+Prob_str="CuPoQ"
+VISLIST="Sim Str"
+RELIST="R0 R100 R200 R500 R1000"
+PRECLIST="WedNe WedNlFePe"
+ANGLIST="Ax0y0z0 Ax30y30z30"
+NOELLIST="N4 N6 N8 N10 N12 N14"
+
+for PREC in $PRECLIST
+do
+  for VIS in $VISLIST
+  do
+    for ANG in $ANGLIST
+    do
+      for RE in $RELIST
+      do
+        LINE=""
+        for NOEL in $NOELLIST
+        do
+
+          RESFILE="${Prob_str}${VIS}${RE}${PREC}${ANG}${NOEL}NP1R0"
+          TOKEN=""
+
+          if [ -f $RESFILE ]
+          then
+            TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
+          else
+            TOKEN="x"
+          fi
+          
+          LINE="$LINE $TOKEN"
+        done
+        echo $LINE
+      done
+    done
+  done
+done
+}
+
+function format_cube_amg()
+{
+ITSTIMEDIR="res_iterations"
+
+cd $ITSTIMEDIR
+
+TAG="RAYAVGITS"
+#TAG="RAYPRECSETUP"
+#TAG="RAYLINSOLVER"
+
+LINE=""
+
+Prob_str="CuPoQ"
+VISLIST="Sim Str"
+RELIST="R0 R100 R200 R500 R1000"
+PRECLIST="WedNe WedNlFrayPa"
+ANGLIST="Ax0y0z0 Ax30y30z30"
+NOELLIST="N4 N6 N8 N10 N12 N14 N16"
+
+for PREC in $PRECLIST
+do
+  for VIS in $VISLIST
+  do
+    for ANG in $ANGLIST
+    do
+      for RE in $RELIST
+      do
+        LINE=""
+        for NOEL in $NOELLIST
+        do
+
+          RESFILE="${Prob_str}${VIS}${RE}${PREC}${ANG}${NOEL}NP1R0"
+          TOKEN=""
+
+          if [ -f $RESFILE ]
+          then
+            TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
+          else
+            TOKEN="x"
+          fi
+          
+          LINE="$LINE $TOKEN"
+        done
+        echo $LINE
+      done
+    done
+  done
+done
+}
+
+
+cd $CURRENT_DIR
+echo -e "\n"
+echo -e "Formatting EXACTS results"
+format_cube_exacts
+
+cd $CURRENT_DIR
+echo -e "\n"
+echo -e "Formatting pure AMG results"
+format_cube_amg
+}
+
+FORMAT_RESULTS_SCRIPT="format_results.sh"
+echo '#!/bin/bash' >> $FORMAT_RESULTS_SCRIPT
+declare -f local_format_results >> $FORMAT_RESULTS_SCRIPT
+echo 'local_format_results' >> $FORMAT_RESULTS_SCRIPT
+echo -e "\n I have created the format results script: $FORMAT_RESULTS_SCRIPT"
+echo -e "\n"
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+################### Now check if I'm on csf, if so, delete the related scratch
+# and copy the current stuff there.
+echo -e "\n"
+OOMPH_ABS_ROOT_DIR=""
+SCRATCH_ABS_ROOT_DIR=""
+COPY_TO_SCRATCH=0
+
+if [[ $HOME == *mbax5ml3* ]]
+then
+  OOMPH_ABS_ROOT_DIR="/mnt/iusers01/mh01/mbax5ml3/oomphlib_optimized"
+  SCRATCH_ABS_ROOT_DIR="/mnt/iusers01/mh01/mbax5ml3/scratch/oomphlib_optimized"
+  echo "This is csf, copying files into:"
+  echo "$SCRATCH_ABS_ROOT_DIR"
+  COPY_TO_SCRATCH=1
+elif [[ $HOSTNAME == *onigiri* ]]
+then
+  OOMPH_ABS_ROOT_DIR="/home/ray/oomphlib/matthias_bpf_rewrite"
+  SCRATCH_ABS_ROOT_DIR="/home/ray/oomphlib/scratch/oomphlib_optimized"
+  echo "This is onigiri, testing of files into:"
+  echo "$SCRATCH_ABS_ROOT_DIR"
+  COPY_TO_SCRATCH=1
+else
+  echo "Unrecognised machine, not doing copy into scratch."
+  COPY_TO_SCRATCH=0
+fi
+
+echo -e "\n"
+
+# NOTE, we are still in TEST_DIR
+if [ "$COPY_TO_SCRATCH" -eq "1" ]
+then
+  echo "Doing copy to scratch!"
+
+  cd $PROGRAM_DIR
+  LOCAL_PROGRAM_DIR=${PWD##*/}
+  cd $TEST_DIR
+
+  OOMPH_PROGRAM_DIR="$OOMPH_ABS_ROOT_DIR/user_drivers/$LOCAL_PROGRAM_DIR"
+  SCRATCH_PROGRAM_DIR="$SCRATCH_ABS_ROOT_DIR/user_drivers/$LOCAL_PROGRAM_DIR"
+
+  OOMPH_TEST_DIR="$OOMPH_PROGRAM_DIR/$TEST_DIR"
+  SCRATCH_TEST_DIR="$SCRATCH_PROGRAM_DIR/$TEST_DIR"
+
+  echo -e "\n"
+  echo "OOMPH_TEST_DIR: $OOMPH_TEST_DIR"
+  echo "SCRATCH_TEST_DIR: $SCRATCH_TEST_DIR"
+  echo "Removing SCRATCH_TEST_DIR and recopying"
+
+  # Remove the scratch stuff.
+  rm -rf $SCRATCH_TEST_DIR
+  mkdir -p $SCRATCH_TEST_DIR
+
+  rsync -av $OOMPH_TEST_DIR/$PROGRAM $SCRATCH_TEST_DIR/
+  rsync -av $OOMPH_TEST_DIR/$QSUBFILE $SCRATCH_TEST_DIR/
+  rsync -av $OOMPH_TEST_DIR/$TEST_LIST $SCRATCH_TEST_DIR/
+  rsync -av $OOMPH_TEST_DIR/$FORMAT_RESULTS_SCRIPT $SCRATCH_TEST_DIR/
+
+  ## Create the res_its and qsub output directories in scratch.
+  mkdir -p $SCRATCH_TEST_DIR/$RESITS_DIR
+  QSUBOUTPUT_DIR="qsub_output_$TESTLIST_FILEBASE"
+  mkdir -p $SCRATCH_TEST_DIR/$QSUBOUTPUT_DIR
+  echo -e "\n"
+  echo "I have moved the files:"
+  echo "$PROGRAM"
+  echo "$QSUBFILE"
+  echo "$TEST_LIST"
+  echo "$FORMAT_RESULTS_SCRIPT"
+  echo "and created directories:"
+  echo "$RESITS_DIR"
+  echo "$QSUBOUTPUT_DIR"
+fi
+
 
 
 
