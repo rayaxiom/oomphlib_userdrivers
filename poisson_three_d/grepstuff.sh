19b6cb7c5cc65fc7fcdda22a149a0408e1b7ea41
Raymon White <raymonwhite42@gmail.com>
2014-08-19 19:51:41 +0100

    Fixed typo in grepstuff.sh script.

--- poisson_three_d/grepstuff.sh
+++ poisson_three_d/grepstuff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grep "Processor $1:" $2 | grep "Time to generate Jacobian\|Setting up BoomerAMG\|Time for preconditioner setup\|Time to generate Trilinos matrix\|    Linear solver iterations\|Time for trilinos solve itself\|Time for complete trilinos solve\|Time for linear solver\|Total time for linear solver\|    Total time for Newton solver\|Time outside linear solver"
+grep "Processor $1:" $2 | grep "Time to generate Jacobian\|Setting up BoomerAMG\|Time for preconditioner setup\|Time to generate Trilinos matrix\|Linear solver iterations\|Time for trilinos solve itself\|Time for complete trilinos solve\|Time for linear solver\|Total time for linear solver\|Total time for Newton solver\|Time outside linear solver"
 
 
