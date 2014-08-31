8ddd7f445fa31e8610e8cf4f805f55096f770766
Raymon White <raymonwhite42@gmail.com>
2014-08-31 14:41:33 +0100

    Added qsub and gen test scripts for AnnularWedge 3D

--- lagrange_annular_wedge_3d/test1_f_calibration/format_res.sh
+++ lagrange_annular_wedge_3d/test1_f_calibration/format_res.sh
@@ -1,49 +1,59 @@
 #!/bin/bash
 set -u
 
-TAG="RAYAVGAVGITS"
-PREC="WeNlFa2v2Strn0-75cRSGsPe"
+CURRENTDIR=`pwd`
+
+## Change this for each folder.
+RESDIR=""
+PREC=""
+TAG=""
+
+function get_results()
+{
+
 ############################################
+cd $RESDIR
+
 
 LINE=""
 VISC="Sim"
 
-RESFILE="CuPo${VISC}R200${PREC}A30N4*"
+RESFILE="Aw3D${VISC}R200${PREC}N4*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N6*"
+RESFILE="Aw3D${VISC}R200${PREC}N6*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N8*"
+RESFILE="Aw3D${VISC}R200${PREC}N8*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N10*"
+RESFILE="Aw3D${VISC}R200${PREC}N10*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N12*"
+RESFILE="Aw3D${VISC}R200${PREC}N12*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N14*"
+RESFILE="Aw3D${VISC}R200${PREC}N14*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N16*"
+RESFILE="Aw3D${VISC}R200${PREC}N16*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N18*"
+RESFILE="Aw3D${VISC}R200${PREC}N18*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
@@ -52,47 +62,150 @@ echo $LINE
 LINE=""
 
 VISC="Str"
-RESFILE="CuPo${VISC}R200${PREC}A30N4*"
+RESFILE="Aw3D${VISC}R200${PREC}N4*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N6*"
+RESFILE="Aw3D${VISC}R200${PREC}N6*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N8*"
+RESFILE="Aw3D${VISC}R200${PREC}N8*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N10*"
+RESFILE="Aw3D${VISC}R200${PREC}N10*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N12*"
+RESFILE="Aw3D${VISC}R200${PREC}N12*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N14*"
+RESFILE="Aw3D${VISC}R200${PREC}N14*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N16*"
+RESFILE="Aw3D${VISC}R200${PREC}N16*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
-RESFILE="CuPo${VISC}R200${PREC}A30N18*"
+RESFILE="Aw3D${VISC}R200${PREC}N18*"
 TOKEN=$(grep "$TAG" $RESFILE | awk '{print $NF}')
 LINE="$LINE $TOKEN"
 
 
 echo $LINE
+
+cd $CURRENTDIR
+
+
+}
+
+
+#RESDIR:
+#test1_2v2Strn0-75cRSGs_oomph
+#test2_2v2Strn0-75cRSEuclid_oomph
+#test3_2v2Strn0-25cRSGs_oomph
+#test4_2v2Strn0-50cRSGs_oomph
+#test5_2v2Strn0-80cRSGs_oomph
+#test6_2v2Strn0-90cRSGs_oomph
+#test7_2v2Strn0-668cRSGs_oomph
+
+#PREC:
+#WeNlFa2v2Strn0-75cRSGsPe
+#WeNlFa2v2Strn0-75cRSEuclidPe
+#WeNlFa2v2Strn0-25cRSGsPe
+#WeNlFa2v2Strn0-5cRSGsPe
+#WeNlFa2v2Strn0-8cRSGsPe
+#WeNlFa2v2Strn0-9cRSGsPe
+#WeNlFa2v2Strn0-668cRSGsPe
+
+#TAG:
+#RAYAVGAVGITS
+#RAYAVGAVGPRECSETUP
+#RAYAVGAVGLINSOLVER
+
+
+TAG="RAYAVGAVGITS"
+
+
+#RESDIR="test3_2v2Strn0-25cRSGs_oomph"
+#PREC="WeNlFa2v2Strn0-25cRSGsPe"
+#get_results
+
+#RESDIR="test4_2v2Strn0-50cRSGs_oomph"
+#PREC="WeNlFa2v2Strn0-5cRSGsPe"
+#get_results
+
+RESDIR="test1_2v2Strn0-75cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-75cRSGsPe"
+get_results
+
+RESDIR="test5_2v2Strn0-80cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-8cRSGsPe"
+get_results
+
+RESDIR="test6_2v2Strn0-90cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-9cRSGsPe"
+get_results
+echo ""
+##########################
+TAG="RAYAVGAVGPRECSETUP"
+
+
+#RESDIR="test3_2v2Strn0-25cRSGs_oomph"
+#PREC="WeNlFa2v2Strn0-25cRSGsPe"
+#get_results
+
+#RESDIR="test4_2v2Strn0-50cRSGs_oomph"
+#PREC="WeNlFa2v2Strn0-5cRSGsPe"
+#get_results
+
+RESDIR="test1_2v2Strn0-75cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-75cRSGsPe"
+get_results
+
+RESDIR="test5_2v2Strn0-80cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-8cRSGsPe"
+get_results
+
+RESDIR="test6_2v2Strn0-90cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-9cRSGsPe"
+get_results
+
+echo ""
+##########################
+TAG="RAYAVGAVGLINSOLVER"
+
+
+#RESDIR="test3_2v2Strn0-25cRSGs_oomph"
+#PREC="WeNlFa2v2Strn0-25cRSGsPe"
+#get_results
+
+#RESDIR="test4_2v2Strn0-50cRSGs_oomph"
+#PREC="WeNlFa2v2Strn0-5cRSGsPe"
+#get_results
+
+RESDIR="test1_2v2Strn0-75cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-75cRSGsPe"
+get_results
+
+RESDIR="test5_2v2Strn0-80cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-8cRSGsPe"
+get_results
+
+RESDIR="test6_2v2Strn0-90cRSGs_oomph"
+PREC="WeNlFa2v2Strn0-9cRSGsPe"
+get_results
+
 
 
 
