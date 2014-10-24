8bd16dbbd80c478d94eb06da4676a474e18650b4
Raymon White <raymonwhite42@gmail.com>
2014-10-24 20:56:47 +0100

    Added new file: new file:   tetgen_files_unit_cube/send_to_wulf.sh

---
+++ milan_permutation/tetgen_files_unit_cube/send_to_wulf.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+DEST="/home/mly/mpi_optimized/user_drivers/milan_permutation/tetgen_files_unit_cube"
+
+scptw cube.1.ele $DEST
+scptw cube.1.face $DEST
+scptw cube.1.node $DEST
+scptw cube.2.ele $DEST
+scptw cube.2.face $DEST
+scptw cube.2.node $DEST
+scptw cube.3.ele $DEST
+scptw cube.3.face $DEST
+scptw cube.3.node $DEST
+scptw cube.4.ele $DEST
+scptw cube.4.face $DEST
+scptw cube.4.node $DEST
+scptw cube.5.ele $DEST
+scptw cube.5.face $DEST
+scptw cube.5.node $DEST
+scptw cube.6.ele $DEST
+scptw cube.6.face $DEST
+scptw cube.6.node $DEST
+scptw cube.7.ele $DEST
+scptw cube.7.face $DEST
+scptw cube.7.node $DEST
+scptw cube.8.ele $DEST
+scptw cube.8.face $DEST
+scptw cube.8.node $DEST
+scptw cube.9.ele $DEST
+scptw cube.9.face $DEST
+scptw cube.9.node $DEST
+scptw cube.10.ele $DEST
+scptw cube.10.face $DEST
+scptw cube.10.node $DEST
+scptw cube.11.ele $DEST
+scptw cube.11.face $DEST
+scptw cube.11.node $DEST
+scptw cube.12.ele $DEST
+scptw cube.12.face $DEST
+scptw cube.12.node $DEST
+scptw cube.13.ele $DEST
+scptw cube.13.face $DEST
+scptw cube.13.node $DEST
+scptw cube.14.ele $DEST
+scptw cube.14.face $DEST
+scptw cube.14.node $DEST
+scptw cube.15.ele $DEST
+scptw cube.15.face $DEST
+scptw cube.15.node $DEST
+scptw cube.16.ele $DEST
+scptw cube.16.face $DEST
+scptw cube.16.node $DEST
+scptw cube.17.ele $DEST
+scptw cube.17.face $DEST
+scptw cube.17.node $DEST
+
+
+
+
+
