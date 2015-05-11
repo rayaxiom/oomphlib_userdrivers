6e3aadbaa72ff541da4929559fde5ef8a71f03b9
Raymon White <raymonwhite42@gmail.com>
2015-05-11 10:30:11 +0100

    Done the get_times.sh shell to use a function. Things should be a cleaner now. Up to #12

--- lagrange_annular_wedge_3d/test3_7_reverted_back_lgrprec_rpl_blocks/test3/from_csf/qsub_output/get_times.sh
+++ lagrange_annular_wedge_3d/test3_7_reverted_back_lgrprec_rpl_blocks/test3/from_csf/qsub_output/get_times.sh
@@ -17,92 +17,163 @@ OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "XX")
 ############################################################################
 ############################################################################
 
+awkit()
+{
+grepstring=$1
+bashcol=$2
+OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "$grepstring")
+echo "${OUTPUT}"
+echo "${OUTPUT}" | awk -v awkcol="$bashcol" '{ print $awkcol }'
+echo "${OUTPUT}" | awk -v awkcol="$bashcol" '{ sum += $awkcol } END { if (NR > 0) print sum / NR }'
+}
 
-#1 - Time to generate Jacobian #############################################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Time to generate Jacobian")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $9 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $9 } END { if (NR > 0) print sum / NR }'
-# CHECKED
 
-#2 - LGR: clean_up_memory: #################################################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LGR: clean_up_memory")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 1
+awkit "Time to generate Jacobian" 9
 
-#3 - BLKSETUP: t_gen_coarsen_mapping #######################################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP: t_gen_coarsen_mapping")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 2
+awkit "LGR: clean_up_memory" 5
 
-#4 - BLKSETUP: t_block_to_dof_map ##########################################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP: t_block_to_dof_map")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 3
+awkit "BLKSETUP: t_gen_coarsen_mapping" 5
 
-#5 - BLKSETUP: t_clear_block_preconditioner_base ###########################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP: t_clear_block_preconditioner_base")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 4
+awkit "BLKSETUP: t_block_to_dof_map" 5
 
-#6 - BLKSETUP: t_initialise_index_in_dof_and_dof_num_sparse ################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP: t_initialise_index_in_dof_and_dof_num_sparse")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 5
+awkit "BLKSETUP: t_clear_block_preconditioner_base" 5
 
-#7 - BLKSETUP_MO: t_get_mesh_ndof_types ####################################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP_MO: t_get_mesh_ndof_types")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 6
+awkit "BLKSETUP: t_initialise_index_in_dof_and_dof_num_sparse" 5
 
-#8 - BLKSETUP_MO_SENDSPARSE: t_create_dense_row_required ###################
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP_MO_SENDSPARSE: t_create_dense_row_required")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 7
+awkit "BLKSETUP_MO: t_get_mesh_ndof_types" 5
 
-#9 - BLKSETUP_MO_SENDSPARSE: t_sparse_global_rows_for_block_lookup #########
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP_MO_SENDSPARSE: t_sparse_global_rows_for_block_lookup")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 8
+awkit "BLKSETUP_MO_SENDSPARSE: t_create_dense_row_required" 5
 
-#10 - BLKSETUP_MO_SENDSPARSE: t_copy_to_global_index_sparse 
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP_MO_SENDSPARSE: t_copy_to_global_index_sparse")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }' ##### DOING HERE?
-# CHECKED
+# 9
+awkit "BLKSETUP_MO_SENDSPARSE: t_sparse_global_rows_for_block_lookup" 5
 
-#11 - BLKSETUP_MO_SENDSPARSE: t_sending_recv_requests_sparse
-#echo "#11 - BLKSETUP_MO_SENDSPARSE: t_sending_recv_requests_sparse"
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP_MO_SENDSPARSE: t_sending_recv_requests_sparse")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 10
+awkit "BLKSETUP_MO_SENDSPARSE: t_copy_to_global_index_sparse" 5
 
-#12 - BLKSETUP_MO: t_setting_and_sending_sparse_vecs
-#echo "#12 - BLKSETUP_MO: t_setting_and_sending_sparse_vecs"
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "BLKSETUP_MO: t_setting_and_sending_sparse_vecs")
-#echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $5 }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
-# CHECKED
+# 11
+awkit "BLKSETUP_MO_SENDSPARSE: t_sending_recv_requests_sparse" 5
+
+# 12
+awkit "BLKSETUP_MO: t_setting_and_sending_sparse_vecs" 5
+
+# 13
+#awkit "" 5
+
+# 14
+#awkit "" 5
+
+# 15
+#awkit "" 5
+
+# 16
+#awkit "" 5
+
+# 17
+#awkit "" 5
+
+# 18
+#awkit "" 5
+
+# 19
+#awkit "" 5
+
+# 20
+#awkit "" 5
+
+# 21
+#awkit "" 5
+
+# 22
+#awkit "" 5
+
+# 23
+#awkit "" 5
+
+# 24
+#awkit "" 5
+
+# 25
+#awkit "" 5
+
+# 26
+#awkit "" 5
+
+# 27
+#awkit "" 5
+
+# 28
+#awkit "" 5
+
+# 29
+#awkit "" 5
+
+# 30
+#awkit "" 5
+
+# 31
+#awkit "" 5
+
+# 32
+#awkit "" 5
+
+# 33
+#awkit "" 5
+
+# 34
+#awkit "" 5
+
+# 35
+#awkit "" 5
+
+# 36
+#awkit "" 5
+
+# 37
+#awkit "" 5
+
+# 38
+#awkit "" 5
+
+# 39
+#awkit "" 5
+
+# 40
+#awkit "" 5
+
+# 41
+#awkit "" 5
+
+# 42
+#awkit "" 5
+
+# 43
+#awkit "" 5
+
+# 44
+#awkit "" 5
+
+# 45
+#awkit "" 5
+
+# 46
+#awkit "" 5
+
+
+
+
+
+
+
+
+
 
 #13 - BLKSETUP_MO: t_problem_distributed
 #echo "#13 - BLKSETUP_MO: t_problem_distributed"
@@ -199,138 +270,117 @@ OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LGR: t_norm_time")
 #echo "${OUTPUT}" | awk '{ sum += $5 } END { if (NR > 0) print sum / NR }'
 
 #25 - LSC: get block B get_B_time
-echo "#25 - LSC: get block B get_B_time"
-bashcol=8
-OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: get block B get_B_time")
-echo "${OUTPUT}"
-echo "${OUTPUT}" | awk -v awkcol="$bashcol" '{ print $awkcol }' ## This is to be deleted
-echo "${OUTPUT}" | awk -v awkcol="$bashcol" '{ sum += $awkcol } END { if (NR > 0) print sum / NR }'
+#echo "#25 - LSC: get block B get_B_time"
+#bashcol=8
+#OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: get block B get_B_time")
+#echo "${OUTPUT}"
+#echo "${OUTPUT}" | awk -v awkcol="$bashcol" '{ print $awkcol }' ## This is to be deleted
+#echo "${OUTPUT}" | awk -v awkcol="$bashcol" '{ sum += $awkcol } END { if (NR > 0) print sum / NR }'
 
 #26 - LSC: ivmm_assembly_time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: ivmm_assembly_time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #27 - LSC: get block Bt
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: get block Bt")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #28 - LSC: t_QBt_time (matrix multiplicaton)
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: t_QBt_time (matrix multiplicaton)")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #29 - LSC: t_p_time (matrix multiplication)
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: t_p_time (matrix multiplication)")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #30 - LSC: QBt (setup MV product)
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: QBt (setup MV product)")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #31 - LSC: get_block t_get_F_time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: get_block t_get_F_time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #32 - LSC: MV product setup t_F_MV_time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: MV product setup t_F_MV_time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #33 - LSC: get_block t_get_Bt_time2
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: get_block t_get_Bt_time2")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #34 - LSC: MV product setup t_Bt_MV_time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: MV product setup t_Bt_MV_time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #35 - LSC: p_prec setup time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: p_prec setup time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #36 - LSC: f_prec setup time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LSC: f_prec setup time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #37 - LGR: ns_setup
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LGR: ns_setup")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #38 - LGR: delete_v_aug
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LGR: delete_v_aug")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #39 - LGR: t_w_prec_time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LGR: t_w_prec_time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #40 - LGR: delete_w time
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "LGR: delete_w time")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #41 - Time for preconditioner setup
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Time for preconditioner setup")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #42 - Time to generate Trilinos matrix
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Time to generate Trilinos matrix")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #43 - Linear solver iterations
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Linear solver iterations")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #44 - Time for trilinos solve itself
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Time for trilinos solve itself")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #45 - Time for complete trilinos solve
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Time for complete trilinos solve")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 #46 - Time for linear solver
+bashcol=8
 OUTPUT=$(grep "Processor 0" ${RSLTFILE} | grep "Time for linear solver")
 #echo "${OUTPUT}"
-#echo "${OUTPUT}" | awk '{ print $YY }' ## This is to be deleted
-#echo "${OUTPUT}" | awk '{ sum += $YY } END { if (NR > 0) print sum / NR }'
 
 
 
