d112f9ab6ad9b2622c50e1672e996a808d0a2e26
Raymon White <raymonwhite42@gmail.com>
2015-12-31 07:39:42 +0000

    modified:   newtest05_Str_strn_ogmres.sh max iter to 500
    Changes to be committed:
    modified:   newtest05_Str_strn_ogmres.sh

--- lagrange_square/newtest05_Str_strn_ogmres.sh
+++ lagrange_square/newtest05_Str_strn_ogmres.sh
@@ -55,9 +55,7 @@ TEST_LIST=""
 
 
 # Declare generic params here.
-#PARAM="--dist_prob --prob_id 11  --max_solver_iter 300 --itstimedir $RESITS_DIR --solver_type 2 --print_hypre --w_solver 0 --ns_solver 1 --p_solver 0 --f_solver 96 --f_amg_iter 1 --f_amg_smiter 2 --f_amg_coarse 1 --f_amg_sim_smoo 1 --f_amg_str 0.25 --f_amg_damp 0.1 --ang 30"
-PARAM="--dist_prob --prob_id 11  --max_solver_iter 300 --itstimedir $RESITS_DIR --solver_type 1 --print_hypre --w_solver 0 --ns_solver 1 --p_solver 0 --f_solver 96 --f_amg_iter 1 --f_amg_smiter 2 --f_amg_coarse 1 --f_amg_sim_smoo 1 --ang 30"
-#PARAM="--dist_prob --prob_id 11  --max_solver_iter 300 --itstimedir $RESITS_DIR --solver_type 2 --print_hypre --w_solver 0 --ns_solver 1 --p_solver 0 --ang 30"
+PARAM="--dist_prob --prob_id 11  --max_solver_iter 500 --itstimedir $RESITS_DIR --solver_type 1 --print_hypre --w_solver 0 --ns_solver 1 --p_solver 0 --f_solver 96 --f_amg_iter 1 --f_amg_smiter 2 --f_amg_coarse 1 --f_amg_sim_smoo 1 --ang 30"
 
 
 function gen_tests()
