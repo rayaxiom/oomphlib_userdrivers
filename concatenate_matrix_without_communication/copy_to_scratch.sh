1105c2a3407b473753d755f0b33e12ffadbe755c
Raymon White <raymonwhite42@gmail.com>
2016-01-07 07:46:23 +0000

    Changes to be committed: modified:   copy_to_scratch.sh

--- concatenate_matrix_without_communication/copy_to_scratch.sh
+++ concatenate_matrix_without_communication/copy_to_scratch.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 PROGRAM="mat_cat"
-QSUBFILE="strong_matcat_np16.qsub"
-TESTLIST="strong_testlist_np16.list"
+STRONGQSUBFILE="strong_matcat_np16.qsub"
+STRONGTESTLIST="strong_testlist_np16.list"
+
+WEAKQSUBFILE="weak_matcat_np16.qsub"
+WEAKTESTLIST="weak_testlist_np16.list"
+
 
 SCRATCHDIR="/mnt/iusers01/mh01/mbax5ml3/scratch/mpi_optimized/user_drivers/concatenate_matrix_without_communication"
 
 make $PROGRAM && 
 cp $PROGRAM $SCRATCHDIR/ && \
-cp $QSUBFILE $SCRATCHDIR/ && \
-cp $TESTLIST $SCRATCHDIR
+cp $STRONGQSUBFILE $SCRATCHDIR/ && \
+cp $STRONGTESTLIST $SCRATCHDIR/ && \
+cp $WEAKQSUBFILE $SCRATCHDIR/ && \
+cp $WEAKTESTLIST $SCRATCHDIR/
 
 
 
