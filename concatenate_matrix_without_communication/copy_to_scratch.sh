a2e9d67e80682bb0e93fbb666a13fbc1e377113c
Raymon White <raymonwhite42@gmail.com>
2016-01-07 05:39:40 +0000

    Testing where time is going for matrix concatenate without communication
    Changes to be committed:
    modified:   copy_to_scratch.sh
    new file:   s2trong.qsub
    modified:   strong_scaling.qsub

--- concatenate_matrix_without_communication/copy_to_scratch.sh
+++ concatenate_matrix_without_communication/copy_to_scratch.sh
@@ -3,7 +3,8 @@ PROGRAM="mat_cat"
 
 SCRATCHDIR="/mnt/iusers01/mh01/mbax5ml3/scratch/mpi_optimized/user_drivers/concatenate_matrix_without_communication"
 
-make $PROGRAM && cp $PROGRAM $SCRATCHDIR/
+make $PROGRAM && cp $PROGRAM $SCRATCHDIR/ \
+cp strong_test_list.list $SCRATCHDIR/
 
 
 
