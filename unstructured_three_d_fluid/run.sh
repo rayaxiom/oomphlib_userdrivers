232314ee05805a1df7b59ca49a965bf8a2f7db3e
Raymon White <raymonwhite42@gmail.com>
2016-10-11 19:53:40 +0100

    Initial commit for unstructured 3d driver code. I have only put in commandline flags, nothing else.
    Changes to be committed:
    modified:   .gitignore
    new file:   unstructured_three_d_fluid/Makefile.am
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid.1.ele
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid.1.face
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid.1.node
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid.poly
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid_with_extended_tubes.1.ele
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid_with_extended_tubes.1.face
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid_with_extended_tubes.1.node
    new file:   unstructured_three_d_fluid/fsi_bifurcation_fluid_with_extended_tubes.poly
    new file:   unstructured_three_d_fluid/run.sh
    new file:   unstructured_three_d_fluid/three_d_unstructured_fluid.mcr
    new file:   unstructured_three_d_fluid/unstructured_three_d_fluid.cc

---
+++ unstructured_three_d_fluid/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+PROG="unstructured_three_d_fluid"
+RUNCOMMAND="mpirun -np 1"
+
+LINSOLVER="--use_trilinos"
+NSSOLVER="--use_lsc"
+FSOLVER="--use_amg_for_f"
+PSOLVER="--use_amg_for_p"
+VISCTERM="--use_stress_div"
+RE="--re 50.0"
+DOCDIR="--doc_dir RESLT"
+DOCNUM="--doc_num 0"
+DOCLABEL="--doc_label soln"
+
+PARAM="${LINSOLVER}"
+PARAM="${PARAM} ${NSSOLVER} ${FSOLVER} ${PSOLVER} "
+PARAM="${PARAM} ${VISCTERM} ${RE} "
+PARAM="${PARAM} ${DOCDIR} ${DOCNUM} ${DOCLABEL}"
+
+make ${PROG} && \
+$RUNCOMMAND ./$PROG $PARAM
+
+
+
